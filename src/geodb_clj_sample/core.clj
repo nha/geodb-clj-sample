bf7ac9b8586b3e70384b13cfa033df52e51d8a31
nha <nha@users.noreply.github.com>
2018-02-05 00:04:28 +0000

    feat: boilerplate

--- src/geodb_clj_sample/core.clj
+++ src/geodb_clj_sample/core.clj
@@ -1,7 +1,42 @@
 (ns geodb-clj-sample.core
-  (:gen-class))
+  (:gen-class)
+  (:require [geodb.driver :as geodb]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, GeoDB!")
+
+  ;; TODO with-open
+
+  (let [driver (geodb/make-driver
+                 {:host      "api.geodb.io"        ;; default
+
+                  ;; TODO use these
+                  :reconnect-timeout 5000
+                  :exp-backoff-ceiling 60000
+
+                  ;; TODO data format to get back
+                  :packer :edn      ;; default to EDN
+                  :type :ws         ;; only type supported for now
+                  :protocol :https  ;; secure by default
+                  :callback-timeout 10000}
+                 )]
+
+    ;; connect
+    ;; note: the Driver is a simple Record, and is therefore immutable
+    ;;
+    (geodb/connect driver
+                   {:UserId    "<your-user-id-here>"
+                    :ApiKey    "<you-api-key-here>"})
+
+    ;; subscribe
+
+
+    ;; publish
+
+    ;; get result!
+
+
+    ;; close
+    ))
