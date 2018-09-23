d70f52fc5bc615b4d057d87cbd3bf06d2c3f9f85
nha <nha@users.noreply.github.com>
2018-09-23 22:56:52 +0100

    feat: circle tests

--- src/geodb_clj_sample/core.clj
+++ src/geodb_clj_sample/core.clj
@@ -1,80 +1,63 @@
 (ns geodb-clj-sample.core
-  (:gen-class)
-  (:require [geodb.driver :as geodb]))
+  (:require [geodb.config]
+            [geodb.protocol]
+            [geodb.driver :as driver]
+            [geodb.create]
+            [geodb.api :as geodb]))
 
 (defn -main
   [& args]
-  (println "Hello, GeoDB!")
-  (println "Open the source file and REPL in to follow the tutorial")
-  )
+  (println "Hello, GeoDB! " {:awesome geodb.api/awesome
+                             :version geodb.api/version
+                             :commitVersion geodb.api/commitVersion
+                             :buildNum geodb.api/buildNum})
+  (println "Open the source file and REPL in to follow the tutorial"))
 
-;;
-;; A driver represents a connection to the servers
-;;
-
-(def driver (geodb/make-driver {:host (or (System/getenv "GEODB_API_HOST") "geodb.io")
-                                :type :ws
-                                :protocol :http ;; TODO :https
-                                :packer :edn
-                                :ws-kalive-ms 20000
-                                :callback-timeout 10000}))
-
-(def connect-p (promise))
-
-;; Register event handlers
-(def driver (-> driver
-                (geodb/on "connect" (fn [evt]
-                                      (println "connect event")
-                                      (deliver connect-p evt)))
-                (geodb/on "error" (fn [evt]
-                                    (println "error event")))
-                (geodb/on "disconnect" (fn [evt]
-                                         (println "disconnected event")))))
 
 (comment
-  (do
+  ;;
+  ;; Evaluate these forms in your REPL
+  ;;
 
-    ;; Now the driver will connect
-    (def driver (geodb/connect driver {:UserToken (java.util.UUID/fromString
-                                                    (System/getenv "GEODB_USER_TOKEN"))
-                                       :ApiKey (java.util.UUID/fromString
-                                                 (System/getenv "GEODB_API_KEY"))}))
+  (geodb.api/init {:host (or (System/getenv "GEODB_API_HOST") "geodb.io")
+                   :type :ws
+                   :protocol :https})
 
-    (deref connect-p)
-    (println "Connected? " (:open? (geodb/connection-state driver))) ;; true
+  (geodb.api/on "ready"
+                (fn [evt]
+                  (println "ready event" evt)))
 
-    (println "Auth-state" (geodb/auth-status driver)) ;; {:status :authenticated}
+  (geodb.api/on "error" (fn [evt]
+                          (println "error event" evt)))
 
-    (def driver (geodb/subscribe driver {:channel "#test"}
-                                 (fn [err data metadata]
-                                   (println "data received on channel #test" err data metadata))))
+  (geodb.api/on "disconnect" (fn [evt]
+                               (println "disconnected event")))
 
-    (def driver (geodb/publish driver {:channel "#test"
-                                       :payload {:msg "anything goes in the payload"}}
-                               (fn publish-callback [err data metadata]
-                                 (println "publish " err "-" data "-" metadata))))
+  (geodb.api/connect {:UserToken (java.util.UUID/fromString
+                                   (System/getenv "GEODB_USER_TOKEN"))
+                      :ApiKey (java.util.UUID/fromString
+                                (System/getenv "GEODB_API_KEY"))})
+
+  (def channel (str "#test-" (System/currentTimeMillis)))
+
+  (geodb.api/subscribe {:channel channel}
+                       (fn [err data metadata]
+                         (println "data received on channel #test" err data metadata)))
 
 
-    ;; try different variations now!
+  (def res (geodb.api/publish {:channel channel
+                               :payload {:ok "true"}}
+                              (fn publish-callback [err data metadata]
+                                (println "data sent on channel #test" err data metadata))))
 
-    (def driver (geodb/subscribe driver {:channel "#test"
-                                         :location {:lat 48.8566
-                                                    :lon 2.3522
-                                                    :radius "50km"
-                                                    :annotation "Paris"}}
-                                 (fn [err data metadata]
-                                   (println "data received in Paris#test" err " - "data " - "metadata))))
 
-    (def driver (geodb/publish driver {:channel "#test"
-                                       :location {:lat 48.8049
-                                                  :lon 2.1204
-                                                  ;;:radius "5km"
-                                                  :annotation "Versailles"}
-                                       :payload {:msg "anything goes in the payload"}}
-                               (fn publish-callback [err data metadata]
-                                 (println "publish " err " - " data " - " metadata))))
-    )
-
-  ;; try these in you REPL!
-  (keys (geodb/list-subscriptions driver))
   )
+
+
+;; This concludes the tutorial :)
+;; You may want to look at the tests too, they include some potentially
+;; interesting patterns
+;;
+;; (def driver (geodb.create/make {:host (or (System/getenv "GEODB_API_HOST") "geodb.io")
+;;                                 :type :ws
+;;                                 :protocol :https}))
--- test/geodb_clj_sample/core_test.clj
+++ test/geodb_clj_sample/core_test.clj
@@ -1,8 +1,82 @@
 (ns geodb-clj-sample.core-test
-  (:require [clojure.test :refer :all]
-            [geodb.driver :as geodb]
-            [geodb.api]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [geodb.api])
+  (:import [java.util.concurrent CompletableFuture]))
+
+(defn- get-token []
+  (java.util.UUID/fromString
+    (System/getenv "GEODB_USER_TOKEN")))
+
+(defn get-api-key []
+  (java.util.UUID/fromString
+    (System/getenv "GEODB_API_KEY")))
+
 
 (deftest geodb-test
+
   (testing "geodb is awesome"
-    (is (true? geodb.api/awesome))))
+    (is (true? geodb.api/awesome)))
+
+  (testing "has a version"
+    (is geodb.api/version)))
+
+
+(deftest geodb-api-test
+
+  (testing "can init"
+    (is (geodb.api/init {:host (or (System/getenv "GEODB_API_HOST") "geodb.io")
+                         :type :ws
+                         :protocol :https})))
+
+  (testing "can connect"
+    (let [connect-p (promise)]
+
+      (geodb.api/on "connect"
+                    (fn [evt]
+                      (println "connect event")
+                      (deliver connect-p evt)))
+
+      (geodb.api/connect {:UserToken (get-token)
+                          :ApiKey (get-api-key)})
+
+      (is (not= ::timeout (deref connect-p 1000 ::timeout)))
+      (when (realized? connect-p)
+        (is (= (select-keys (second @connect-p) [:auth])
+               {:auth {:status :authenticated}})))))
+
+
+  (testing "can pubsub"
+    (let [payload {:msg "hello"
+                   :d (java.util.Date.)}
+          channel (str "#test-" (System/currentTimeMillis))
+          sub-p (promise)
+          pub-p (promise)
+          ^CompletableFuture sub-res (geodb.api/subscribe {:channel channel}
+                                       (fn [err data metadata]
+                                         (println "data received on channel #test" err data metadata)
+                                         (deliver sub-p {:err err :data data :metadata metadata})))]
+
+      (is (not= ::timeout (deref sub-res 1000 ::timeout)))
+
+      (when (.isDone sub-res)
+        (let [^CompletableFuture publish-res (geodb.api/publish {:channel channel
+                                                                 :payload payload}
+                                                                (fn publish-callback [err data metadata]
+                                                                  (deliver pub-p {:err err :data data :metadata metadata})))]
+
+          (is (not= ::timeout (deref publish-res 1000 ::timeout)))
+          (is (not= ::timeout (deref sub-p 1000 ::timeout)))
+
+          (is (= (:data (deref publish-res 0 ::timeout))
+                 (:data (deref pub-p 0 ::timeout))))
+
+          (when (realized? sub-p)
+            (is (nil? (:err @sub-p)))
+            (is (= payload (:data @sub-p))))
+
+          (is (not= ::timeout (deref pub-p 1000 ::timeout)))
+          (when (realized? pub-p)
+            (is (nil? (:err @pub-p)))
+            (is (= 1 (:publishedCount (:data @pub-p)))))))))
+
+  )
