1f3216fba82ca88007191574f42da979eec99c2b
Nicolas Ha <nha@users.noreply.github.com>
2018-12-20 22:00:27 +0000

    ci run tests (#4)

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.nha/geodb-clj-sdk "0.0.1"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojars.nha/geodb-clj-sdk "0.0.9"]]
   :main ^:skip-aot geodb-clj-sample.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :ci {:dependencies  ^:replace [[org.clojure/clojure "1.9.0"]
-                                            [org.clojars.nha/geodb-clj-sdk  #=(eval (System/getenv "CI_GEODB_VERSION"))]]}})
+             :ci {:dependencies  ^:replace [[org.clojure/clojure "1.10.0"]
+                                            [org.clojars.nha/geodb-clj-sdk #=(eval (System/getenv "CI_GEODB_VERSION"))]]}})
---
+++ scripts/ci_local_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# used by the integration tests outside of this repository
+set -e
+
+boot clojure-driver-jar
